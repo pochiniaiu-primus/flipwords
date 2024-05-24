978d655699b4efbd61260643e5443d70c7740cab
Serhii Honchar <serhii.honchar.java@gmail.com>
2024-05-24 21:28:55 +0200

    create new flash cards

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from tkinter import *
-
+import pandas
+import random
 BACKGROUND_COLOR = "#B1DDC6"
+data = pandas.read_csv("data/german_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="German")
+    canvas.itemconfig(card_word, text=current_card["German"])
+
 
 window = Tk()
 window.title("Flashy")
@@ -10,17 +20,19 @@ canvas = Canvas(width=800, height=526)
 front_image = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=front_image)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
 check_image = PhotoImage(file="images/right.png")
 
-unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(column=0, row=1)
 
-known_button = Button(image=check_image, highlightthickness=0)
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(column=1, row=1)
+
+next_card()
 
 window.mainloop()
